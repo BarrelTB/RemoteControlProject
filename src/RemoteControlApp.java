3fc0dd7dc3b10cf8a2ab626475aba5f28a06035d
Caleb Whitford <calebwhitford@Administrators-MacBook-Pro-2.local>
2019-11-08 16:43:11 -0700

    Added TODOs

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -3,7 +3,10 @@ public class RemoteControlApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
 	}
-
+		// TODO: Add pressButton() method
+		// TODO: Add turn on button
 }
