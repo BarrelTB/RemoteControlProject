ebd2f9232c8dab2234350c0d42a2e8e53dd9b2bc
Caleb Whitford <calebwhitford@Administrators-MacBook-Pro-2.local>
2019-11-08 17:22:21 -0700

    Added do while loop and turnOn method

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -4,11 +4,24 @@ public class RemoteControlApp {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
+		String button;
 		
-		kb.close();
+		button = "";
 		
-
+		do {
+			System.out.println("To begin, turn on the remote control:");
+			button = kb.next();
+			
+		} while (!button.equals("ON"));
+		turnOn();
+		
+	
 	}
-		// TODO: Add pressButton() method
-		// TODO: Add turn on button
+
+	public static void pressButton() {
+		System.out.println("BOOP!");
+	}
+	public static void turnOn() {
+		System.out.println("TURNING ON...");
+	}
 }
